2b225e3631518d877b0a7b39a66170c3179cd9e0
Viktor NAGY <viktor.nagy@pulispace.com>
2018-11-24 16:10:31 +0100

    Renamed project folder.

--- SoftarchShapefile/ShapefileEditor/Properties/Resources.Designer.cs
+++ SoftarchShapefile/ShapefileEditor/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace ShapefileEditor.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DockingMap.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShapefileEditor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
